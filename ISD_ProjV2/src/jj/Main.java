a450181712355af170a558c46678aa474dcd8530
TANQxx <tank-0-pl@hotmail.com>
2018-11-20 12:26:28 +0100

    TF IDF, Euq
    dodalem 2 tabele

--- ISD_ProjV2/src/jj/Main.java
+++ ISD_ProjV2/src/jj/Main.java
@@ -2,6 +2,7 @@ package jj;
 
 import java.awt.BorderLayout;
 import java.awt.Checkbox;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.TextArea;
@@ -13,6 +14,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -21,18 +23,20 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Main {
 
-	public static JPanel PanelLVL1 = new JPanel();
+		   static JPanel PanelLVL1;
 		   static File chosenFile;
 		   static JLabel LabelwybranyPlikNazwa;
 		   static TextArea PoleNaWynikSzukaniaTermow;
 		   static JTextField PoleNaTermy;
 		   static Checkbox precyzja;
+		   static JTextField EuqTargetTextF;
 		   
 		   boolean PRE;
 		   
@@ -40,6 +44,13 @@ public class Main {
 		   static String IlewystN = "";
 		   
 		   static int LdwTSM = 0;
+		   static int IleRoznychTermow = 0;
+		   
+		   static DaneTablicy DTab;
+		   
+		   static JTable TTSM;
+		   static JTable TIFIDF;
+		   static JTable TEuq;
 		   
 	
 	
@@ -47,10 +58,11 @@ public class Main {
 	{
 		JFrame Program = new JFrame("ISD Projekt");
 		
-		Program.setSize(new Dimension(1200, 500));
-		//Program.setResizable(false);
+		Program.setSize(new Dimension(1200, 600));
+		Program.setResizable(false);
 		Program.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		PanelLVL1 = new JPanel();
 		Program.add(PanelLVL1);
 		
 		Content();
@@ -71,7 +83,7 @@ public class Main {
 		PanelLVL3L.setMaximumSize(new Dimension(500, 1000));
 		
 		//Lewa czesc zawierajaca 1 cz programu (wczytywanie plikow i slow)
-		//TODO zabespieczenie przed nie wczytaniem pliku & podaniem termow
+		
 		
 		PanelLVL3L.setLayout(new BoxLayout(PanelLVL3L, BoxLayout.PAGE_AXIS));
 		
@@ -82,7 +94,7 @@ public class Main {
 		WPspec.add(wskazywaniePliku);
 
 		wskazywaniePliku.addActionListener(new ActionListener() {
-			//TEST2
+			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser chooser= new JFileChooser();
@@ -117,11 +129,25 @@ public class Main {
 		JButton BPrzeszukaj = new JButton("Skanuj plik w poszukiwaniu termów");
 		PanelLVL3L.add(BPpec);
 		BPpec.add(BPrzeszukaj);
+		
 		BPrzeszukaj.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				przeszukiwaniePliku();			
+				try {
+				if (chosenFile.exists())
+				{
+					if (PoleNaTermy.getText().isEmpty())
+					{
+						PoleNaTermy.setText("PODAJ TERMY!");
+					}
+					else
+						przeszukiwaniePliku();
+				}
+				}catch(NullPointerException brakPliku)
+				{
+					PoleNaWynikSzukaniaTermow.setText("Wczytaj plik!");
+				}							
 			}
 		});
 		
@@ -147,65 +173,51 @@ public class Main {
 		JPanel PanelLVL2P = new JPanel();
 		PanelLVL1.add(PanelLVL2P, BorderLayout.LINE_END);
 		
-		//TODO Prawa cz zawierajaca obrobke 2 cz programu
+		//Prawa cz zawierajaca obrobke 2 cz programu
 		
 		JPanel P2PMenu = new JPanel();
 		PanelLVL2P.setLayout(new BorderLayout());
 		PanelLVL2P.add(P2PMenu, BorderLayout.NORTH);
 		
 		//Menu prawej strony
-		
-		
-		JButton DodawanieDoTSM = new JButton("Dodaj do mTSM");
+				
+		JButton DodawanieDoTSM = new JButton("Dodaj do mTFM");
 		P2PMenu.add(DodawanieDoTSM);
-		
-		
-		JButton ResetowanieTSM = new JButton("Resetuj TSM");
+				
+		JButton ResetowanieTSM = new JButton("Resetuj TFM");
 		P2PMenu.add(ResetowanieTSM);
+			
+		TTSM = new JTable(DTab.daneTSM, DTab.naglowki);
 		
-		
-		String[][] dane = 
-				{	{"","","","","","","","",""},
-				    {"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""},
-					{"","","","","","","","",""}	};
-		String[] naglowki = {"","","","","","","","",""};
-		JTable TTSM = new JTable(dane, naglowki);
 		JPanel PL2Ptsm = new JPanel();
 		PanelLVL2P.add(PL2Ptsm, BorderLayout.CENTER);
 		PL2Ptsm.setLayout(new BoxLayout(PL2Ptsm, BoxLayout.PAGE_AXIS));
 		
-		JLabel P2PtsmLabel = new JLabel("TSM");
-		PL2Ptsm.add(P2PtsmLabel);
-		
+		JLabel P2PtsmLabel = new JLabel("TFM");
+		PL2Ptsm.add(P2PtsmLabel);		
 		PL2Ptsm.add(TTSM);
 		
-		//TODO scrolowalny panel z odpowiedziami
-		
-		JPanel PL2Pscrol = new JPanel();		
-		
-		PL2Pscrol.setPreferredSize(new Dimension( 700,1300)); //TODO 
-		
-		JScrollPane ScrollowanyPanelnaTablice = new JScrollPane(PL2Pscrol);
-		
-		ScrollowanyPanelnaTablice.setPreferredSize(new Dimension( 720,300));
-		
-		PanelLVL2P.add(ScrollowanyPanelnaTablice, BorderLayout.SOUTH);
-		
-		
+		ResetowanieTSM.addActionListener(new ActionListener() {
+
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				LdwTSM = 0;
+				
+				for (int i = 0 ; i<11 ; i++) 
+					for(int ii = 0 ; ii<9 ; ii++ ) 
+						TTSM.setValueAt("",i,ii);
+								
+				PoleNaTermy.setEditable(true);				
+			}
+		});
 		
 		DodawanieDoTSM.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO dodawanie wyniku do tsm
+				//dodawanie wyniku do tsm
 				
 				if (LdwTSM == 0)
 				{
@@ -225,21 +237,94 @@ public class Main {
 					LdwTSM++;
 				}});
 		
-		ResetowanieTSM.addActionListener(new ActionListener() {
+		PL2Ptsm.add(Box.createRigidArea(new Dimension(15, 15)));
+		
+		JSeparator SeparowanieTSM = new JSeparator();
+		SeparowanieTSM.setOrientation(SwingConstants.HORIZONTAL);
+		PL2Ptsm.add(SeparowanieTSM);
+		
+		//scrolowalny panel z odpowiedziami
+		
+		JPanel PL2Pscrol = new JPanel();				
+		PL2Pscrol.setLayout(new BoxLayout(PL2Pscrol, BoxLayout.PAGE_AXIS));
+		PL2Pscrol.setPreferredSize(new Dimension( 700,1300)); 		
+		JScrollPane ScrollowanyPanelnaTablice = new JScrollPane(PL2Pscrol);		
+		ScrollowanyPanelnaTablice.setPreferredSize(new Dimension( 720,300));		
+		PanelLVL2P.add(ScrollowanyPanelnaTablice, BorderLayout.SOUTH);
+		
+		JPanel TFIDFButtonPanel = new JPanel();
+		PL2Pscrol.add(TFIDFButtonPanel);
+		TFIDFButtonPanel.setLayout(new BoxLayout(TFIDFButtonPanel, BoxLayout.LINE_AXIS));
+		
+		
+		JButton ButtonTSMdo1OP = new JButton("TFM > TF-IDF");
+		TFIDFButtonPanel.add(ButtonTSMdo1OP);
+		
+		ButtonTSMdo1OP.addActionListener(new ActionListener() {		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				obliczanieIfIdf();
+			}
+		});
+		
+		JButton ButtonResetTFIDF = new JButton("Resetuj tablice TF-IDF");
+		TFIDFButtonPanel.add(ButtonResetTFIDF);
+		
+		ButtonResetTFIDF.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				LdwTSM = 0;
-				
-				for (int i = 0 ; i<7 ; i++) 
-					for(int ii = 0 ; ii<9 ; ii++ ) 
-						TTSM.setValueAt("",i,ii);
-								
-				PoleNaTermy.setEditable(true);				
+				for (int i = 0; i < LdwTSM; i++)
+					for (int ii = 0 ; ii < (int)IleRoznychTermow+1 ; ii++)
+						TIFIDF.setValueAt("", i, ii);
+			}
+		});
+			
+		TIFIDF = new JTable(DTab.daneTFIFD, DTab.naglowki);	
+		PL2Pscrol.add(TIFIDF);
+		
+		JPanel EuqlidesButtonPanel = new JPanel();
+		PL2Pscrol.add(EuqlidesButtonPanel);
+		EuqlidesButtonPanel.setLayout(new BoxLayout(EuqlidesButtonPanel, BoxLayout.LINE_AXIS));
+		
+	
+		JLabel EuqTarget = new JLabel("Ktory dokument chcesz porownac");
+		EuqlidesButtonPanel.add(EuqTarget);
+		
+		JPanel EuqTFfix = new JPanel();
+		EuqlidesButtonPanel.add(EuqTFfix);
+		EuqTargetTextF = new JTextField(2);
+		EuqTFfix.setMaximumSize(new Dimension(75, 25));
+		EuqTFfix.add(EuqTargetTextF);
+		
+		JButton EuqStartButton = new JButton("TFM > Euq");
+		EuqlidesButtonPanel.add(EuqStartButton);
+		
+		EuqStartButton.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				obliczanieEuq();
 			}
 		});
 		
+		JButton EuqResetButton = new JButton("Reset Euq");
+		EuqlidesButtonPanel.add(EuqResetButton);
+		
+		EuqResetButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {				
+				for (int i = 0; i < LdwTSM; i++)
+					for (int ii = 0 ; ii < (int)IleRoznychTermow+3 ; ii++)
+						TEuq.setValueAt("", i, ii);				
+			}
+		});
+		
+		TEuq = new JTable(DTab.daneEuq,DTab.Euqnaglowki);
+		PL2Pscrol.add(TEuq);
+		
+		//TODO reszta odpowiedzi programu
 		
 	}
 
@@ -248,14 +333,10 @@ public class Main {
 		PoleNaWynikSzukaniaTermow.setText(null);
 		TermyN = PoleNaTermy.getText();
 		String[] Termy = TermyN.split(Pattern.quote(","));
-		int IleTermow[]= new int[Termy.length];
+		IleRoznychTermow = Termy.length;
+		int IleTermow[]= new int[IleRoznychTermow];
 		int i = 0;
 		IlewystN = "";
-		
-//		for (int term : IleTermow)
-//		{
-//			term = 0;
-//		}
 		
 		StringBuilder contentBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(chosenFile)))
@@ -295,6 +376,91 @@ public class Main {
 			IlewystN=IlewystN+IleTermow[i];
 			if(i < Termy.length-1) IlewystN=IlewystN+",";
 		}
+	}
+	
+	public static void obliczanieIfIdf()
+	{	
+		double[] ileNiePustychTermow = new double[IleRoznychTermow];
+		
+		for (double element : ileNiePustychTermow)
+			ileNiePustychTermow[(int) element] = 0.0;
+		
+		//Zbieram iformacje o tym czy istnieje term w danym dokumencie
+		for (int i = 1; i < LdwTSM; i++)
+			for (int ii = 1 ; ii < IleRoznychTermow ; ii++)
+			{
+				if (TTSM.getValueAt(i,ii).toString().equals("0"))
+				{}else
+				{
+					ileNiePustychTermow[ii-1] ++;
+				}
+			}
+		
+		//Obliczam wartosci dla tablicy IFiDF
+		for (int i = 0; i < LdwTSM; i++)
+			for (int ii = 0 ; ii < (int)IleRoznychTermow+1 ; ii++)
+			{
+				if (ii == 0 || i == 0) {
+				if (ii == 0) {
+					TIFIDF.setValueAt(TTSM.getValueAt(i, ii), i, ii);	
+				}
+				if (i == 0) {
+					TIFIDF.setValueAt(TTSM.getValueAt(i, ii), i, ii);
+				}
+				}else 
+				{
+					Double wynik = Double.parseDouble((String) TTSM.getValueAt(i,ii)) * Math.log((LdwTSM-1)/ileNiePustychTermow[ii-1]);
+					TIFIDF.setValueAt(Double.toString(wynik),i,ii);
+				}
+			}
+		
+	}
+	
+	public static void obliczanieEuq() {
+		
+		int indexDok = Integer.parseInt(EuqTargetTextF.getText());
+		int sumaTermow = 0;
+		
+		
+		for (int i = 0; i < LdwTSM; i++)
+			for (int ii = 0 ; ii < (int)IleRoznychTermow+3 ; ii++)
+			{
+				if (ii == 0 || i == 0) {
+				if (ii == 0) {
+					TEuq.setValueAt("D" + indexDok + " - D" + i, i, ii);	
+				}
+				if (i == 0) {
+					if ( ii < (int)IleRoznychTermow+1)
+					TEuq.setValueAt(TTSM.getValueAt(i, ii), i, ii);
+					else
+					if (ii == (int)IleRoznychTermow+1)
+					TEuq.setValueAt("Suma", i, ii);
+					else
+					if (ii == (int)IleRoznychTermow+2)
+					TEuq.setValueAt("Sqrt(Suma)", i, ii);
+				}
+				}else 
+				{
+					if ( ii < (int)IleRoznychTermow+1)
+						{
+						Double wynik = Math.pow((Double.parseDouble((String) TTSM.getValueAt(indexDok,ii))-Double.parseDouble((String) TTSM.getValueAt(i,ii))), 2);
+						TEuq.setValueAt(Double.toString(wynik),i,ii);
+						sumaTermow += wynik;
+						}
+						else
+						if (ii == (int)IleRoznychTermow+1)
+						TEuq.setValueAt(Integer.toString(sumaTermow), i, ii);
+						else
+						if (ii == (int)IleRoznychTermow+2)
+						{
+						TEuq.setValueAt(Double.toString(Math.sqrt(sumaTermow)), i, ii);
+						sumaTermow = 0;
+						}
+						
+				}
+			}
+		
+		
 	}
 	
 	public static void main(String[] args) 
